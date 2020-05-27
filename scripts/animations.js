fe6be2b049b3ffca2923087d09505b983ef88c4f
Tomas <tomvargasd@gmail.com>
2020-05-26 23:15:15 -0500

    add loader page

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,2 +1,12 @@
 // Animations init
-new WOW().init();
+
+var spage;
+
+function timer() {
+  spage = setTimeout(show, 3000);
+}
+
+function show() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page").style.display = "block";
+}
