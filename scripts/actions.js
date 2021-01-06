37f524d40a53d5f2a7d8eb94b496b58b92a34901
tomvargas <tomvargasd@gmail.com>
2021-01-06 11:25:22 -0500

    fix navbar toggler

---
+++ scripts/actions.js
@@ -0,0 +1,9 @@
+function showhid(){
+    var x = document.getElementById("navbarSupportedContent-7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+   
+  } else {
+    x.style.display = "none";
+  }
+}
