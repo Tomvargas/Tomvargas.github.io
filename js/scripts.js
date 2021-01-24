7bae56cf9b43773726f07125f44b07c186b5eaa4
tomvargas <tomvargasd@gmail.com>
2021-01-24 17:56:56 -0500

    upload the new look page

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function change_theme(){
+
+    var light = './css/light.css';
+    var dark = './css/dark.css';
+    var page = document.getElementById('style');
+    var pageatt = page.getAttribute('href');
+
+    if (pageatt === dark){
+        page.href=light;
+    }else{
+        page.href=dark;
+    }
+}
+
+function show_loader(){
+    document.getElementById('loader').style.display='flex';
+    setTimeout(()=>{
+        document.getElementById('loader').style.display='none';
+    }, 4000)
+}
+
+function show_lang(){
+    var element = document.getElementById('Lang');
+    var element_att = element.getAttribute('style');
+    if (element_att === "display: none;"){
+        element.style.display='flex';
+    }else{
+        element.style.display='none';
+    }
+}
